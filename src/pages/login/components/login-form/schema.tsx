418d55746724c985a92c7f7a3005121942bc447d
Omiadze <teona.omiadze.1@iliauni.edu.ge>
2025-09-27 22:22:50 +0400

    fix form error issue

--- src/pages/login/components/login-form/schema.tsx
+++ src/pages/login/components/login-form/schema.tsx
@@ -1,6 +1,11 @@
 import { z } from "zod";
 
 export const LoginFormSchema = z.object({
-  email: z.string().email({ message: "Please enter a valid email address." }),
-  password: z.string().min(1, { message: "Password is required." }),
+  email: z
+    .string()
+    .min(3, { message: "Email must be at least 3 characters long." })
+    .email({ message: "Please enter a valid email address." }),
+  password: z
+    .string()
+    .min(3, { message: "Password must be at least 3 characters long." }),
 });
--- src/pages/registration/components/register-form/schema.tsx
+++ src/pages/registration/components/register-form/schema.tsx
@@ -2,14 +2,19 @@ import { z } from "zod";
 
 export const RegisterFormSchema = z
   .object({
-    email: z.string().email({ message: "Please enter a valid email address." }),
-    username: z.string().min(1, { message: "Username is required." }),
+    email: z
+      .string()
+      .min(3, { message: "Email must be at least 3 characters long." })
+      .email({ message: "Please enter a valid email address." }),
+    username: z
+      .string()
+      .min(3, { message: "Username must be at least 3 characters long." }),
     password: z
       .string()
-      .min(6, { message: "Password must be at least 6 characters long." }),
+      .min(3, { message: "Password must be at least 3 characters long." }),
     password_confirmation: z
       .string()
-      .min(6, { message: "Please confirm your password." }),
+      .min(3, { message: "Please confirm your password (min 3 characters)." }),
   })
   .refine((data) => data.password === data.password_confirmation, {
     message: "Passwords do not match.",
